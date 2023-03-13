b107167229fe50e4b281328ed10202df02b8b311
Adam Schubert <adamgschubert@gmail.com>
2023-03-13 14:49:25 -0500

    initalize branch

---
+++ scripts/database.js
@@ -0,0 +1,20 @@
+let database = {
+    sections:[
+        {
+            id: 1,
+            section: "Northeast"
+        },
+        {
+            id: 2,
+            section: "Northeast"
+        },
+        {
+            id: 3,
+            section: "Northeast"
+        },
+        {
+            id: 4,
+            section: "Northeast"
+        },
+    ]
+}
