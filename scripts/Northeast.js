1d8152842c8420ceeb0e11801bdd60f771db2659
sergey-mik <intelsurge@gmail.com>
2023-03-15 14:30:33 -0500

    fixes

--- scripts/Northeast.js
+++ scripts/Northeast.js
@@ -41,23 +41,7 @@ export const Northeast = () => {
   return html
 }
 
-//click event listener
-document.addEventListener('click', (clickEvent) => {
-  const itemClicked = clickEvent.target
-  if (itemClicked.id.startsWith('section2')) {
-    //splits the string apart into an array to have the primery key of an object
-    const [, sectionId] = itemClicked.id.split('--')
-    let totalGuests = 0
-    //find the whole object by iterating
-    for (const guest of guests) {
-      //
-      if (guest.sectionId === parseInt(sectionId)) {
-        totalGuests++
-      }
-    }
-    window.alert(`There are ${totalGuests} guests in this area`)
-  }
-})
+
 
 export const ServicesNortheast = () => {
   let html = '<h4> Park services: '
