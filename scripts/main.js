00a9deac0f0fb1d38dc862b8c30a7fb26b286156
sergey-mik <intelsurge@gmail.com>
2023-03-16 16:08:32 -0500

    main fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,8 @@
-// import { ServicesNortheast } from './Northeast.js'
-// import { ServicesNorthern } from './Northern.js'
 import { southEastHtml } from './southeast.js'
 import { southernHtml } from './southern.js'
 import { northEastHtml } from './Northeast.js'
 import { northernHtml } from './Northern.js'
 import {} from './eventHandler.js'
-
-import { Northeast, ServicesNortheast } from './Northeast.js'
-import { Northern, } from './Northern.js'//ServicesNorthern
-import { southEastHtml } from './southeast.js'
-import { southernHtml } from './southern.js'
 import { } from './eventHandler.js'
 import { guestList, serviceList } from './lists.js'
 import { northWestHtml } from './northwest.js'
@@ -17,9 +10,6 @@ import { southWestHtml } from './Southwest.js'
 
 const mainContainer = document.querySelector('#container')
 
-{
-    /* <img src="./images/" class="logo" /> */
-}
 const applicationHTML = `
         <header class="header">
         
@@ -29,24 +19,22 @@ const applicationHTML = `
         <div class="marquee">Park Services ${serviceList()}</div>
         <aside class="guests">Guests  ${guestList()}</aside>
 
-<article class="details">
-    <section class="detail--column details_section">
         
 <article class="details">
     <section class="detail--column details_section">
         <h2>Northeast</h2>
         ${northEastHtml()}
-
     </section>
     <section class="detail--column details_section">
         <h2>Northern</h2>
-        ${Northern()}
+        ${northernHtml()}
     </section>
     <section class="detail--column details_section">
         <h2>Northwest</h2>
         ${northWestHtml()}
     </section>
 </article>
+
 <article class="details">
     <section class="detail--column details_section">
         <h2>Southwest</h2>
